45268dbaebcf48e999f303a53b561f2602757cae
devmikh <mishacyb@gmail.com>
2022-06-28 14:51:50 -0700

    Add comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Die from './components/Die';
 import { nanoid } from 'nanoid';
 import Confetti from 'react-confetti';
+import Header from './components/Header';
+import Die from './components/Die';
 
 export default function App() {
 
+  /*
+    State for holding currently displayed dice.
+    Initialized with an array of newly created dice objects with random values
+  */
   const [dice, setDice] = React.useState(createNewDice());
+
+  // Holds boolean value to determine whether user has won or not
   const [hasWon, setHasWon] = React.useState(false);
+
+  /*
+    State for holding best rolls count of the user. 
+    Initialized with bestRolls value from localStorage. If it's not there, defaults to 0
+  */
   const [bestRolls, setBestRolls] = React.useState(localStorage.getItem("bestRolls") || 0);
+
+  /*
+    State for holding current rolls count of the user.
+    Initialized with 0
+  */
   const [currentRolls, setCurrentRolls] = React.useState(0);
 
+  /*
+    Checks if all elements in dice array are selected and equal to each other.
+    If yes, then user has won.
+    Triggers on every change of the dice state
+  */
   React.useEffect(() => {
     const allSelected = dice.every(die => die.isSelected);
     const allEqual = dice.every(die => die.value === dice[0].value);
@@ -20,6 +41,9 @@ export default function App() {
     }
   }, [dice])
 
+  // Helper functions
+
+  // Create single random die object
   function createRandomDie() {
     return {
       id: nanoid(),
@@ -28,6 +52,7 @@ export default function App() {
     }
   }
 
+  // Create an array of random die objects
   function createNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
@@ -36,11 +61,20 @@ export default function App() {
     return newDice;
   }
 
+  // Change isSelected value of the die based on its id
   function selectDie(id) {
     setDice(oldDice => oldDice.map(oldDie => oldDie.id === id ? { ...oldDie, isSelected: !oldDie.isSelected} : oldDie));
   }
 
+  /* 
+    Button handler
+  */
   function rollDice() {
+    /*
+      'New Game' button functionality (when hasWon is true)
+      Sets new rolls record, if it's better than the previous best score;
+      Sets the game in its initial state: hasWon now is false, new dice are created, current rolls set to 0
+    */
     if (hasWon) {
       if (bestRolls === 0 || currentRolls < bestRolls) {
         setBestRolls(currentRolls);
@@ -49,12 +83,18 @@ export default function App() {
       setHasWon(false);
       setDice(createNewDice());
       setCurrentRolls(0);
+    /* 
+      'Roll' button functionality (when hasWon is false)
+      Roll all dice except for the ones that are currently selected
+      Add 1 to the current rolls count
+    */
     } else {
       setDice(oldDice => oldDice.map(oldDie => oldDie.isSelected ? oldDie : createRandomDie()));
       setCurrentRolls(oldRolls => oldRolls + 1);
     }
   }
 
+  // Create dice elements from dice array
   const diceElements = dice.map(die => {
     return <Die
               key={die.id}
@@ -83,7 +123,6 @@ export default function App() {
             <h3>Best: {bestRolls}</h3>
           </div>
         </div>
-        
       </main>
     </div>
   );
